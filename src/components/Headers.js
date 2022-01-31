2aeb19b1c009264bb5ac5cab0cfd30fd09c5040d
Katie Nolan <nolakat@gmail.com>
2022-01-31 10:48:05 -0500

    Add Typography

---
+++ src/components/Headers.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Headers = () => {
+  return (
+  <div>
+    <h1 class="text-4xl">The quick brown fox jumps over the lazy dog</h1>
+    <h2 class="text-3xl">The quick brown fox jumps over the lazy dog</h2>
+    <h3 class="text-2xl">The quick brown fox jumps over the lazy dog</h3>
+    <h4 class="text-xl">The quick brown fox jumps over the lazy dog</h4>
+    <h5 class="text-lg">The quick brown fox jumps over the lazy dog</h5>
+  </div>
+  );
+};
+
+
